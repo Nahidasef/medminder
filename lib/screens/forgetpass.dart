85d3a813973ecc6becf009f737df7f307737cd51
Nahidasef <naas50dx@gmail.com>
2023-12-06 13:11:43 +0600

    firsb

---
+++ lib/screens/forgetpass.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgetPass extends StatefulWidget {
+  const ForgetPass({super.key});
+
+  @override
+  State<ForgetPass> createState() => _ForgetPass();
+}
+
+class _ForgetPass extends State<ForgetPass> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Forget Password');
+  }
+}
